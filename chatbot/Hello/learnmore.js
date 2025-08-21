e224dab8e161b18b37f8d33f07f5ecdc18a36b9d
shibam104 <sshibam2004@gmail.com>
2025-08-22 02:25:14 +0530

    updated at 02:25

---
+++ chatbot/Hello/learnmore.js
@@ -0,0 +1,9 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
